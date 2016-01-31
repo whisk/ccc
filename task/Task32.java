6f9ba9a1460d834aeea3827b5f02c3e6c6a80295
Alex Svetkin <alex.svetkin@gmail.com>
2016-01-31 23:54:15 +0000

    write to cassandra

--- task/Task32.java
+++ task/Task32.java
@@ -23,17 +23,23 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.NoHostAvailableException;
+import com.datastax.driver.core.exceptions.QueryExecutionException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Formatter;
+import java.lang.Math;
 
 import common.ImprovedTask;
-import common.TextArrayWritable;
-import common.Pair;
 import common.mr.ReduceAverage;
-import common.mr.MapPairKeyMinList;
-import common.mr.ReducePairKeyMinList;
 
 public class Task32 extends ImprovedTask implements Tool {
 
@@ -68,28 +74,63 @@ public class Task32 extends ImprovedTask implements Tool {
     }
 
     public static class OriginDestinationDateDepTimeArrDelayMap extends Mapper<Object, Text, Text, FloatWritable> {
-        private Logger log = Logger.getLogger(this.getClass());
+        private Session session;
+        private Cluster cluster;
+        private String query = "insert into ccc_1.trips (origin, destination, departure_date, departure_time, departure_delay ) values (?, ?, ?, ?, ?);";
+        private PreparedStatement prepStmt;
+
+        public Session getSession() {
+            if (this.session.isClosed()) {
+                this.session = this.cluster.connect();
+            }
+
+            return this.session;
+        }
+
+        public void createConnection(String node)  {
+            this.cluster = Cluster.builder().addContactPoint(node).build();
+            Metadata metadata = cluster.getMetadata();
+            System.out.printf("Connected to cluster: %s\n",metadata.getClusterName());
+            for ( Host host : metadata.getAllHosts() ) {
+                System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n", host.getDatacenter(), host.getAddress(), host.getRack());
+            }
+            this.session = cluster.connect();
+        }
+
+        @Override
+        public void setup(Context context) {
+            createConnection("10.0.223.125");
+            Session session = this.getSession();
+            this.prepStmt = session.prepare(this.query); 
+        } 
 
         @Override
         public void map(Object lineNum, Text value, Context context) throws IOException, InterruptedException {
             String[] row = value.toString().split("\\s");
-            try {
-                String year = row[0];
-                if (year.equals("2008")) {
+            Session session = this.getSession();
+//            try {
+                if (row.length > 9 && row[0].equals("2008")) {
+                    String year = row[0];
                     String origin = row[5];
                     String destination = row[6];
                     Formatter formatter = new Formatter(new StringBuilder());
-                    String date = formatter.format("%04d:%02d:%02d", Integer.parseInt(row[0]), Integer.parseInt(row[1]), Integer.parseInt(row[2])).toString();
+                    String date = formatter.format("%04d-%02d-%02d", Integer.parseInt(row[0]), Integer.parseInt(row[1]), Integer.parseInt(row[2])).toString();
                     String time = row[7];
-                    Float arrDelay = Float.parseFloat(row[9]);
+                    int arrDelay = Math.round(Float.parseFloat(row[9]));
                     
                     String key = (origin + "_" + destination + "_" + date + "_" + time).toUpperCase();
                     context.write(new Text(key), new FloatWritable(arrDelay));
+                    
+                    session.execute(this.prepStmt.bind(origin, destination, date, Integer.parseInt(time), arrDelay));
                 }
-            } catch (Exception e) {
+/*            } catch (Exception e) {
                 // skip on error parsing
-                log.error(e);
             }
+*/        }
+
+        @Override
+        public void cleanup(Context context) {
+            cluster.close();
         }
     }
 }
