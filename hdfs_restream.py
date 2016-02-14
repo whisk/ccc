59e01d60ef4e8397ada1bce5104164a64c401725
Alex Svetkin <alex.svetkin@gmail.com>
2016-02-14 22:13:28 +0000

    no Popen

--- hdfs_restream.py
+++ hdfs_restream.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-import subprocess
-from subprocess import Popen
 import hdfs
 import time
 import re
@@ -11,18 +9,8 @@ hdfs_path = '/ccc/input/'
 hdfs_url  = 'http://hadoop-master:50070/'
 
 hdfs_client = hdfs.client.InsecureClient(hdfs_url)
-
-p = Popen(["hdfs", "dfs", "-ls", hdfs_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-code = p.wait()
-if code != 0:
-  print("Error with HDFS path %s: %s" % (hdfs_path, p.stderr.readlines()))
-  sys.exit(1)
-
-for line in p.stdout.readlines():
-  cols = re.split(r'\s+', line.decode().strip())
-  if len(cols) < 7:
-    continue
-  fname_curr = cols[7]
+for fname in hdfs_client.list(hdfs_path):
+  fname_curr = hdfs_path + fname
   fname_new = fname_curr
   m = re.search(r'___(\d{5})$', fname_curr)
   if m:
@@ -39,5 +27,4 @@ for line in p.stdout.readlines():
   t = int(time.time()) * 1000
   print("Touching %s" % fname_curr)
   hdfs_client.set_times(fname_curr, access_time=t, modification_time=t)
-  time.sleep(0.5)
-
+  time.sleep(1)
