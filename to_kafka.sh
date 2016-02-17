6c87e74d4f8be1aec9f9c3803c21f92ef662d40f
Alex Svetkin <svetkin@inventos.ru>
2016-02-17 21:51:23 +0300

    pattern

--- to_kafka.sh
+++ to_kafka.sh
@@ -1,21 +1,23 @@
 #/bin/bash
 
 DATASET_DIR="/dataset_clean"
-BANDWIDTH=40 # Mbps
+BANDWIDTH=55 # Mbps
 TOPIC="ccc_1"
 PARTITIONS=2
 REPLICATION_FACTOR=1
 ZOOKEEPER="kafka-1:2181"
 BROKERS="kafka-1:9092"
+PATT="*2008*.txt"
 
 # delete topic
 kafka-topics.sh --zookeeper $ZOOKEEPER --topic $TOPIC --delete
+sleep 1
 
 # create again
 kafka-topics.sh --zookeeper $ZOOKEEPER --topic $TOPIC --partitions $PARTITIONS --replication-factor $REPLICATION_FACTOR --create
 
 # now produce
-for fname in $DATASET_DIR/*.txt; do
+for fname in $DATASET_DIR/$PATT; do
 	echo "$fname -> kafka"
 	cat $fname | kafka-console-producer.sh --broker-list $BROKERS --topic $TOPIC &
 	size=`stat --format "%s" $fname`
